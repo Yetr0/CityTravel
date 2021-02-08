c9f0c4ce59d3901ba406897e572742c9bb87ad38
Lucas <lucas.altesson@hotmail.com>
2021-02-08 17:50:40 +0100

    fetch attractions now works again

--- code.js
+++ code.js
@@ -1,12 +1,14 @@
 const button = document.getElementById("btn");
+const Attractions = document.getElementById("attractionsResponse");
+const WeatherR = document.getElementById("weatherResponse");
 let searchText = document.getElementById("search-text");
-let Attractions = false;
+let attractions = false;
 let Weather = false;
 let Alphabetically = false;
-const urlToFetch = "https://api.foursquare.com/v2/venues/explore?client_id=FXTNTHIH1BQTKGZUKJ0UU0OGN2O3XBP5TBSLSPRA1RRHPDX3&client_secret=XKW2GPRCYGAMSTT24W15BWCGK01LVG23JAYG33BRFNMYNSFM&v=20210207&limit=10";
+
 
 document.getElementById("fAttrac").onclick = function(){
-    Attractions = Switch(Attractions);
+    attractions = Switch(attractions);
 }
 
 document.getElementById("fWeather").onclick = function(){
@@ -23,50 +25,91 @@ function Switch(value){
 
 button.onclick = async function(){
     try {
-        if(!searchText.value.includes("&"))
-        {
-        const response = await fetch(urlToFetch + "&near=" + searchText.value); 
-        if (response.ok) { 
-          const jsonResponse = await response.json();
-          const attractions = jsonResponse.response.groups[0].items;
-          renderResult(attractions); 
-          console.log(attractions[0].venue.location.address);
-        } else {
-          alert(); 
-        } 
-      }
+        let attractions = await getAttractions();
+        console.log(attractions);
+        renderResult(attractions);
         }
         catch(error) { 
             // Visa felmeddelande för användare 
           }  
 }
+/*<article class="t-center">
+                            <p>Thursday</p>
+                            <p>Temperature: -2&deg;C</p>
+                            <p>Conditions: overcast clouds</p>
+                            <i class="material-icons">clouds</i>
+                        </article> */
 
-function renderResult(list){
+
+
+function renderResult(attractions){
     let attractionHtml = "";
-    for(let i = 0; i < list.length; i++){
+    for(let i = 0; i < attractions.length; i++){
         attractionHtml += 
         `
         <article class="t-center">
-            <p>`+list[i].venue.name+`</p>
+            <p>`+attractions[i].venue.name+`</p>
             <i class="material-icons">free_breakfast</i>
-            <p>Address: `+list[i].venue.location.address+`</p>
+            <a href="http://www.google.com/maps/place/` + attractions[i].venue.location.address + ", " + attractions[i].venue.location.city + `">Address: `+attractions[i].venue.location.address+`</a>
         </article>
-        `
+        `;
     }
-    document.getElementById("city").innerText = searchText.value;
-    document.getElementById("attractionsResponse").innerText = "";
+    /*let weatherHtml = 
+    `
+    <article class="t-center">
+        <p>` + "söndag" + `</p>
+        <p>Temperature: ` + Math.round(weather.main.temp) + `&deg;C</p>
+        <p>Conditions: ` + weather.weather[0].main + `</p>
+        <i class="material-icons">` + getIcon(weather.weather[0].main) + `</i>
+    </article>
+    `;*/
+    let city = searchText.value[0].toUpperCase() + searchText.value.slice(1).toLowerCase();
+    document.getElementById("city").innerText = city;
+    //document.getElementById("weatherResponse").innerHTML = weatherHtml;
     document.getElementById("attractionsResponse").innerHTML = attractionHtml;
     document.getElementById("response").style.visibility = "visible";
-            /*<article class="t-center">
-                <p>Bhoga</p>
-                <i class="material-icons">free_breakfast</i>
-                <p>Address: Norra Hamngatan 10, Göteborg</p>
-            </article>
-            <article class="t-center">
-                <p>The Barn</p>
-                <i class="material-icons">free_breakfast</i>
-                <p>Address: Kyrkogatan 10, Göteborg</p>
-            </article>
-        </div>
-    </section>*/
+}
+
+
+function getIcon(icon){
+    switch(icon){
+        case "Clear":
+            return "sun";
+    }
+}
+
+
+async function getData(urlToFetch){
+    let url = "";
+ if(urlToFetch == 2){
+        url = "https://api.openweathermap.org/data/2.5/weather?&appid=9a3257c2a89bd42ca6b24b5df46f8def&units=metric&q=";
+    }
+        const response = await fetch(url + text); 
+}
+
+async function getAttractions(){
+
+    let url = "https://api.foursquare.com/v2/venues/explore?client_id=FXTNTHIH1BQTKGZUKJ0UU0OGN2O3XBP5TBSLSPRA1RRHPDX3&client_secret=XKW2GPRCYGAMSTT24W15BWCGK01LVG23JAYG33BRFNMYNSFM&v=20210207&limit=10&near=";
+    let text = getSearchText();
+    const response = await fetch(url + text);
+    if (response.ok) { 
+        const jsonResponse = await response.json();
+        console.log(jsonResponse);
+            let attractions = jsonResponse.response.groups[0].items; 
+          return attractions;
+    }
+    else {
+        alert("Something went wrong"); 
+        }
+}
+
+function getSearchText(){
+    if(searchText.value.includes("&")){
+        let text = searchText.value.replace("&","");
+        searchText.value = text;
+        return text;
+    }
+    else{
+        return searchText.value;
+    }
 }
