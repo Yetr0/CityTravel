28b5a18e48be306eb9170f1519e07bb98edb291a
Lucas Altesson <lucas.altesson@learnet.se>
2021-02-15 20:27:57 +0100

    failed fetch now works

--- code.js
+++ code.js
@@ -1,14 +1,13 @@
 const button = document.getElementById("btn");
 let Attractions = document.getElementById("attractions");
 let Weather = document.getElementById("weather");
+let weatherRespone = document.getElementById("weatherResponse");
+let attractionsResponse = document.getElementById("attractionsResponse");
 let searchText = document.getElementById("search-text");
 let attractions = false;
 let weather = false;
 let alphabetically = false;
 let PrevSearch = "";
-let failed = false;
-let weatherRespone = false;
-let attractionResponse = false;
 
 
 document.getElementById("fAttrac").onclick = function(){
@@ -48,30 +47,45 @@ button.onclick = async function(){
         if(prevSearch()){
             attractions = await getAttractions();
             weather = await getWeather();
-            renderAttractions(attractions);
-            renderWeather(weather);
+            if(attractions != null && weather != null){
+                document.getElementById("city").innerText = searchText.value[0].toUpperCase() + searchText.value.slice(1).toLowerCase();
+                renderAttractions(attractions);
+                renderWeather(weather);
+                document.getElementById("failed").style.display = "none";
+            }
+            else{
+                document.getElementById("attractionsResponse").innerHTML = "";
+                document.getElementById("weatherResponse").innerHTML = "";
+                document.getElementById("failed").style.display = "none";
+                document.getElementById("city").innerText = `${searchText.value[0].toUpperCase() + searchText.value.slice(1).toLowerCase()} doesn't exist`;
+            }
         }
-        if(attractionResponse && weatherRespone){
             viewResult();
-        }
         }
         catch(error) {
             document.getElementById("failed").style.display = "block";
+            document.getElementById("city").innerText = "";
+            Weather.style.display = "none";
+            Attractions.style.display = "none";
           }  
 }
 
 function viewResult(){
-    if(weather == true && attractions == false){
+    if(weather == true && attractions == false && weatherRespone.innerHTML != ""){
         Weather.style.display = "block";
         Attractions.style.display = "none";
     }
-    else if(attractions == true && weather == false){
+    else if(attractions == true && weather == false && attractionsResponse.innerHTML != ""){
         Attractions.style.display = "block";
         Weather.style.display = "none";
     }
-    else{
+    else if(attractionsResponse.innerHTML != "" && weatherRespone.innerHTML != ""){
         Attractions.style.display = "block";
         Weather.style.display = "block";
+    }
+    else{
+        Attractions.style.display = "none";
+        Weather.style.display = "none";
     }
 
 }
@@ -90,7 +104,7 @@ function renderAttractions(attractions){
     }
     let city = searchText.value[0].toUpperCase() + searchText.value.slice(1).toLowerCase();
     document.getElementById("city").innerText = city;
-    document.getElementById("attractionsResponse").innerHTML = attractionHtml;
+    attractionsResponse.innerHTML = attractionHtml;
 }
 
 function addressSrc(address, city){
@@ -101,6 +115,8 @@ function addressSrc(address, city){
         return 'href="http://www.google.com/maps/place/' + address + "," + city + "\"";
     }
 }
+
+
 function address (address){
     if(address == undefined){
         return "unknown";
@@ -120,7 +136,7 @@ function renderWeather(weather){
         <i class="material-icons">` + getIcon(weather.weather[0].main) + `</i>
     </article>
     `;
-    document.getElementById("weatherResponse").innerHTML = weatherHtml;
+    weatherRespone.innerHTML = weatherHtml;
 }
 
 //wb_sunny
@@ -144,11 +160,11 @@ async function getWeather(){
         const response = await fetch(url + text);
         if(response.ok){
             const jsonResponse = await response.json();
-            weatherRespone = true;
+            console.log(jsonResponse);
             return jsonResponse;
         }
-        else{
-            weatherRespone = false;
+        if(!response.bodyUsed){
+            return null;
         }
 }
 
@@ -159,11 +175,10 @@ async function getAttractions(){
     const response = await fetch(url + text);
     if (response.ok) { 
         const jsonResponse = await response.json();
-        attractionResponse = true;
             return jsonResponse.response.groups[0].items; 
     }
-    else {
-        attractionResponse = false;
+    else if(!response.bodyUsed) {
+        return null
         }
 }
 
